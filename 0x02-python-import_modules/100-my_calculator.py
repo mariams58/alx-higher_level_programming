c3a1d76f2714465bce6b9ce7c167ef2280208cf1
mariams58 <>
2023-01-08 19:44:35 -0800

    Python modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    op_list = ['+', '-', '*', '/']
+    items = len(sys.argv)
+    a = sys.argv[1]
+    b = sys.argv[3]
+    if items < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if sys.argv[2] not in op_list:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        if sys.argv[2] == op_list[0]:
+            result = add(int(a), int(b))
+        elif sys.argv[2] == op_list[1]:
+            result = sub(int(a), int(b))
+        elif sys.argv[2] == op_list[2]:
+            result = mul(int(a), int(b))
+        elif sys.argv[2] == op_list[3]:
+            result = div(int(a), int(b))
+        print("{} {} {} = {}".format(a, sys.argv[2], b, result))
+
+
+if __name__ == "__main__":
+    main()
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import variable_load_5 as item
+import variable_load_5
 
 
 def main():
-    print("{}".format(item.a))
+    print("{}".format(variable_load_5.a))
 
 
 if __name__ == "__main__":
