6a8a431a1fbfa22b5d93668b6e3588e9eb2fd877
mariams58 <>
2023-01-08 20:06:42 -0800

    dat stuctures

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+
+def print_list_integer(mylist=[]):
+    for i in range(len(mylist)):
+        print("{}".format(i))
+
+
+if __name__ = "__print_list_integer":
+    print_list_integer(mylist)
