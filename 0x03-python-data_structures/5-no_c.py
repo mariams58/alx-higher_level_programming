241e68556ce04183f3ac742bc2ab68ba645b0d71
mariams58 <>
2023-01-08 20:58:51 -0800

    Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    for i in my_string:
+        if i != 'c' or i 1= 'C':
+            print("{}".format(i), end='')
+    print()
+
+
+if __name__ == "__no_c__":
+    no_c(my_string)
