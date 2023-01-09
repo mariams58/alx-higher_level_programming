d4e9c9a05c4dbc7ef52bba77a514a1e6b93f2340
mariams58 <>
2023-01-08 17:45:00 -0800

    Python modules: Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    num_arg = len(sys.argv)
+    if num_arg == 1:
+        print("{} arguments.".format(num_arg -1))
+    elif num_arg > 1:
+        if num_arg > 2:
+            print("{} arguments:".format(num_arg - 1))
+        elif num_arg == 2:
+            print("{} argument:".format(num_arg - 1))
+        for i in range(1, num_arg):
+            print("{}: {}".format(i, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    main()
