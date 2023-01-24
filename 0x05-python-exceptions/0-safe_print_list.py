164fac8d8eeeb71b7e8a1620ed98961c7d908fae
mariams58 <>
2023-01-23 19:25:14 -0800

    Task 0: bugfix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
     num = 0
     try:
         d_list = my_list[:x]
-        print(d_list)
     except:
         d_list = my_list[:-1]
-        print(d_list)
     for item in d_list:
         num += 1
+        print(item, end='')
+    print()
     return num
