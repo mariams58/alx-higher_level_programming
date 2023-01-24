f0f1f217ab27357cbd7ff2ed1b9c189df882a8c2
mariams58 <>
2023-01-23 18:53:28 -0800

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for item in mylist:
+            num += 1
+        d_list = my_list[:x]
+        return x
+    except RangeError:
+        d_list = my_list[:num]
+        return num
+    print d_list
