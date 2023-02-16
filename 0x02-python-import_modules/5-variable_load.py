121b30ae0da99bf0c8ce7cc9beec8215660fe6dd
mariams58 <>
2023-02-16 06:11:36 -0800

    import fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-import variable_load_5
-
-
-def main():
-    x = variable_load_5.a
-    print("{}".format(x))
-
-
 if __name__ == "__main__":
-    main()
+    from variable_load_5 import a
+    print(a)
