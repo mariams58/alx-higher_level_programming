12ac399377dc3ab24a7d95c2ae05b5d95982dce3
mariams58 <>
2023-02-15 17:58:02 -0800

    inset in a sorted list

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@
 
 def fizzbuzz():
     for x in range(1, 101):
-        if x % 3 == 0:
-            x = "Fizz"
+        if x % 3 == 0 and x % 5 == 0:
+            x = "FizzBuzz"
         elif x % 5 == 0:
             x = "Buzz"
-        elif x % 5 == 0 and x % 3 == 0:
-            x = "FizzBuzz"
+        elif x % 3 == 0:
+            x = "Fizz"
         print("{}".format(x), end=' ')
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * insert_node - inserts  a given number in sorted list
+  * @head: the head of the linked list given
+  * @number: numder to insert
+  *
+  * Return: node of insertion
+  */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, temp;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		if (*head == NULL)
+		{
+			new_node->next = NULL;
+			new_node->n = number;
+			*head = new_node;
+			return (*head);
+		}
+		{
+			while (*head != NULL)
+			{
+				temp = (*head)->next;
+				if ((*head)->n < n && temp->n > n)
+				{
+					new_node->n = number;
+					new_node->next = temp;
+					temp = new_node
+				}
+				*head = temp;
+			}
+		}
+		return (new_node);
+		free(new_node->next);
+		free(new_node);
+	}
+	return (NULL);
+}
