603d3eef1eadf961d3ab0f5f1798de062fcd5d4b
mariams58 <mariams58@yahoo.com>
2024-04-26 07:06:58 -0700

    python network0 - task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to 0.0.0.0:5000/catch_men and responds with a message
-curl -sLXH PUT "Origin: X-School-User-Id" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sX PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" "0.0.0.0:5000/catch_me"
