6a97d1b3eded955bf4e545fa31fa4e16c74655b9
mariams58 <mariams58@yahoo.com>
2024-08-21 11:58:37 +0100

    fix: 5.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@
 def def no_c(my_string):
     n_str = ""
     for i in my_string:
-        if i != 'c' and i != 'C':
+        if i != 'c' or i != 'C':
             n_str += i
     return n_str
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 
 def max_integer(my_list=[]):
     if my_list:
-        max = 0
+        max = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > max:
                 max = my_list[i]
