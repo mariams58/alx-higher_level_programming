91f4bc5717f3d865f213e007d53a348d3206adc4
mariams58 <mariams58@yahoo.com>
2024-08-22 09:48:36 +0100

    0x05 fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,9 @@
 def safe_print_division(a, b):
     try:
         res = a / b
+        print("Inside result: {:.1f}".format(res))
     except (TypeError, ZeroDivisionError):
         res = None
+        print("Inside result: {}".format(res))
     finally:
         return res
